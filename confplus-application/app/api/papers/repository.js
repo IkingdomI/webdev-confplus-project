2b372829648e3611fbebdb323bf507ef55026489
Mohamed Elawa <relightsweb@gmail.com>
2023-04-22 03:08:09 +0300

    some adds
    Just added a route for papers.json to start submitting and receiving papers

---
+++ confplus-application/app/api/papers/repository.js
@@ -0,0 +1,27 @@
+import {promises as fs} from 'fs';
+
+export async function readPapers(){
+    const data = await fs.readFile("data/papers.json");
+    let papers = JSON.parse(data);
+    // if (author){
+    //     papers = papers.filter((paper) => paper.author === author);
+    // }
+    console.log(papers);
+    if(papers.length>0)
+        return papers;
+    return null;
+}
+
+export async function createPaper(obj){
+    const data = await fs.readFile("data/papers.json");
+    let papers = JSON.parse(data);
+    
+    
+    
+    accounts.push(obj);
+
+    await fs.writeFile(path, JSON.stringify(accounts));
+    return account;
+
+
+}
