826ffacb50d25c51b54f5293a8a3ebb8b1ee7369
ma1801987 <ma1801987@qu.edu.qa>
2023-04-24 02:53:05 +0300

    Eh

--- confplus-application/app/api/sessions/[session]/route.js
+++ confplus-application/app/api/sessions/[session]/route.js
@@ -2,10 +2,68 @@ import * as repo from "../repository.js";
 
 export async function GET(request, {params})
 {
-    const {session} = params;
+	try 
+	{
+		const {session: id} = params;
 
-    return Response.json(
-        { message: `Lol, ${session}` },
-        { status: 200 }
-    );
+		const session = await repo.readSession(id);
+
+		if (session)
+		{
+			return Response.json(
+				session,
+				{ status: 200 }
+			);
+		}
+		else
+		{
+			return Response.json(
+				{ message: "Session not found." },
+				{ status: 404 }
+			);
+		}
+	}
+	catch (error)
+	{
+		console.log(error.message);
+		
+		return Response.json(
+			{ message: "Internal Server Error." },
+			{ status: 500 }
+		);
+	}
+}
+
+export async function PATCH(id, obj)
+{
+	try 
+	{
+		const {session: id} = params;
+
+		const session = await repo.readSession(id);
+
+		if (session)
+		{
+			return Response.json(
+				session,
+				{ status: 200 }
+			);
+		}
+		else
+		{
+			return Response.json(
+				{ message: "Session not found." },
+				{ status: 404 }
+			);
+		}
+	}
+	catch (error)
+	{
+		console.log(error.message);
+		
+		return Response.json(
+			{ message: "Internal Server Error." },
+			{ status: 500 }
+		);
+	}
 }
--- confplus-application/app/api/sessions/repository.js
+++ confplus-application/app/api/sessions/repository.js
@@ -4,8 +4,16 @@ const path = "data/schedule.json"
 
 export async function readSessions()
 {
-    const res = await fs.readFile(path);
-    const sessions = await JSON.parse(res);
+	const res = await fs.readFile(path);
+	const sessions = await JSON.parse(res);
 
-    return sessions;
+	return sessions;
+}
+
+export async function readSession(id)
+{
+	const res = await fs.readFile(path);
+	const session = await JSON.parse(res).find(s => s.session === id);
+
+	return session;
 }
