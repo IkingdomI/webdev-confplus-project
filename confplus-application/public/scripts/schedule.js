ba6141437e1a704abce77df71fe8e51819b4b096
ma1801987 <ma1801987@qu.edu.qa>
2023-04-26 12:22:37 +0300

    Update schedule.js

--- confplus-application/public/scripts/schedule.js
+++ confplus-application/public/scripts/schedule.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded',async ()=>{
 	const organizerName = document.querySelector("#organizer-name");
   
 	const id = sessionStorage.getItem("user-id");
-	const userRes = await fetch(`http://localhost:3000/api/users/${id}`);
+	const userRes = await fetch(`/api/users/${id}`);
 	const user = await userRes.json();
   
 	organizerName.textContent = `${user.first_name} ${user.last_name}`
 
   
   
-	const locRes = await fetch("http://localhost:3000/api/locations");
+	const locRes = await fetch("/api/locations");
 	const locations = await locRes.json();
 	const locSelect = document.querySelector('#location');
 
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded',async ()=>{
 		locSelect.appendChild(option)
 	})
 
-	const dateRes = await fetch("http://localhost:3000/api/conference-dates");
+	const dateRes = await fetch("/api/conference-dates");
 	const dates = await dateRes.json();
 	const dateSelect = document.querySelector('#date');
 
@@ -33,6 +33,30 @@ document.addEventListener('DOMContentLoaded',async ()=>{
 		option.text = date.date;
 		dateSelect.appendChild(option)
 	})
+
+	async function updateSession(title, obj)
+	{
+		const session = await (await fetch(`/api/sessions/${title}`, { method: "PATCH", body: JSON.stringify(obj)})).json();
+
+		console.log(session);
+
+		if (session)
+		{
+			if (session.message)
+			{
+				alert(session.message);
+			}
+			else
+			{
+				loadSchedule();
+				alert("Session Updated!");
+			}
+		}
+		else
+		{
+			alert("Session not found.");
+		}
+	}
 
 	async function loadSchedule()
 	{
@@ -62,38 +86,82 @@ document.addEventListener('DOMContentLoaded',async ()=>{
 			presentHeading.innerText = `Presented by: ${s.presenter}`;
 			sessionCont.appendChild(presentHeading);
 
+			const lDiv = document.createElement("div");
+			sessionCont.appendChild(lDiv);
+
+			const lLabel = document.createElement("label");
+			lLabel.innerText = "Location: ";
+			lDiv.appendChild(lLabel);
+
+			const lSelect = document.createElement("select");
+			const lIndex = locations.findIndex(l => l.value === s.location);
+			locations.forEach((l, i) => {
+				const locOpt = document.createElement("option");
+				locOpt.value = l.value;
+				locOpt.text = l.name;
+				locOpt.selected = (i === lIndex);
+				lSelect.appendChild(locOpt);
+			});
+			lDiv.appendChild(lSelect);
+
+
+			const dDiv = document.createElement("div");
+			sessionCont.appendChild(dDiv);
+
+			const dLabel = document.createElement("label");
+			dLabel.innerText = "Date: ";
+			dDiv.appendChild(dLabel);
+
 			const dSelect = document.createElement("select");
 			const dIndex = dates.findIndex(d => d.date === s.date);
-			console.log(dates);
+			//console.log(dates);
 			dates.forEach((d, i) => {
 				const dateOpt = document.createElement("option");
-				dateOpt.value = i;
+				dateOpt.value = d.date;
 				dateOpt.text = d.date;
 				dateOpt.selected = (i === dIndex);
 				dSelect.appendChild(dateOpt);
 			});
-			sessionCont.appendChild(dSelect);
+			dDiv.appendChild(dSelect);
+
+			const tDiv = document.createElement("div");
+			sessionCont.appendChild(tDiv);
+
+			const tLabel = document.createElement("label");
+			tLabel.innerText = "Time: ";
+			tDiv.appendChild(tLabel);
 
 			const tSelect = document.createElement("select");
 			const tIndex = times.findIndex(t => t.time === s.time);
 			times.forEach((t, i) => {
 				const timeOpt = document.createElement("option");
-				timeOpt.value = i;
+				timeOpt.value = t.time;
 				timeOpt.text = t.time;
 				timeOpt.selected = (i === tIndex);
 				//console.log(t, i);
 				tSelect.appendChild(timeOpt);
 			});
-			sessionCont.appendChild(tSelect);
+			tDiv.appendChild(tSelect);
 
 			const btnsDiv = document.createElement("div");
 			btnsDiv.classList.add("session-btns");
 			sessionDiv.appendChild(btnsDiv);
+			
 
 			const updateBtn = document.createElement("button");
 			updateBtn.classList.add("update-btn");
 			updateBtn.textContent = "Update";
-			updateBtn.addEventListener("click", () => { console.log(s.title) });
+			updateBtn.addEventListener("click", () => {
+				const obj = {
+					location: lSelect.value,
+					date: dSelect.value,
+					time: tSelect.value
+				};
+
+				console.log(s.title, obj);
+
+				updateSession(s.title, obj);
+			});
 			btnsDiv.appendChild(updateBtn);
 
 			const deleteBtn = document.createElement("button");
